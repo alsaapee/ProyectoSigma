17d939e18abda9e884f859bf5a791b3fbd5f4067
alsaapee <125545772+alsaapee@users.noreply.github.com>
2025-10-26 22:58:54 +0100

    Delete src/main directory

--- src/main/java/com/proyectosigma/teams/model/Pairing.java
+++
@@ -1,6 +0,0 @@
-package com.proyectosigma.teams.model;
-public class Pairing {
-  private final String teamA; private final String teamB;
-  public Pairing(String teamA, String teamB){ this.teamA=teamA; this.teamB=teamB; }
-  public String getTeamA(){ return teamA; } public String getTeamB(){ return teamB; }
-}
--- src/main/java/com/proyectosigma/teams/model/TournamentType.java
+++
@@ -1,2 +0,0 @@
-package com.proyectosigma.teams.model;
-public enum TournamentType { SIMPLE, ELIMINACION_DIRECTA, DOBLE }
